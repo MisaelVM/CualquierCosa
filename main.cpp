3c17f3391b2265daa9d460692a7f686b0572062b
EdgarEspinozaPE <64268406+EdgarEspinozaPE@users.noreply.github.com>
2020-06-02 18:25:33 -0500

    Actualizacion main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
+#include <SFML/Graphics.hpp>
+#include "juego.h"
 #include <iostream>
-using namespace std;
+#include <sstream>
+#include <fstream>
+#include "TileMap.h"
+/*int main(){
+    
+    sf::RenderWindow window(sf::VideoMode(1920,1080), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
+    return 0;
+}
+*/
 int main()
 {
-  cout << "rtyfucfhjghju";
+    juego* Arcanus;
+    Arcanus = new juego(800, 600, "Pruebas de Tilemap");
 }
